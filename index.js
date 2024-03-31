12849af51249670129f6f0e55e2ef8cd8dc1f29a
Muhammad Noman <nomanu222555@gmail.com>
2024-03-31 20:38:05 +0500

    first git practice

---
+++ index.js
@@ -0,0 +1,5 @@
+let message = "Hellow and Welcome";
+console.log(message);
+let name = "Muhammad Noman";
+console.log(name);
+export {};
---
+++ index.ts
@@ -0,0 +1,8 @@
+let message = "Hellow and Welcome";
+
+console.log(message);
+ let name ="Muhammad Noman";
+ console.log(name);
+ 
+ 
+ 
